7df2f16a6829d4697fc55b26993f8e0892d975bb
ujval9 <ujvalshah1@gmail.com>
2023-01-06 14:03:25 +0530

    Initial Commit

---
+++ Conversions App/Conversions_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Conversions_AppApp.swift
+//  Conversions App
+//
+//  Created by Ujval Shah on 06/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Conversions_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
